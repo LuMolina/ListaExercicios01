8fe32413ae12a7ec58d1e6403184eb8e6568d280
LuMolina <90227193+LuMolina@users.noreply.github.com>
2021-11-24 09:44:43 -0300

    meu primeiro commit

---
+++ exercicio02/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro valor: ");
+            string? textoValor01 = Console.ReadLine();
+            Console.WriteLine("Digite o segundo valor: ");
+            string? textoValor02 = Console.ReadLine();
+            Console.WriteLine("Digite o terceiro valor: ");
+            string? textoValor03 = Console.ReadLine();
+
+            int valor01 = Convert.ToInt32(textoValor01);
+            int valor02 = Convert.ToInt32(textoValor02);
+            int valor03 = Convert.ToInt32(textoValor03);
+            int maior = 0;
+            if (valor01 > valor02)
+            {
+                if (valor01 > valor03)
+                {
+                    maior = valor01;
+                }
+                else
+                {
+                    maior = valor03;
+                }
+
+
+            }
+            else
+            {
+                if (valor02 > valor03)
+                {
+                    maior = valor02;
+                }
+                else
+                {
+                    maior = valor03;
+                }
+            }
+            int menor = 0;
+if (valor01 < valor02)
+            {
+                if (valor01 < valor03)
+                {
+                    menor = valor01;
+                }
+                else
+                {
+                    menor = valor03;
+                }
+
+
+            }
+            else
+            {
+                if (valor02 < valor03)
+                {
+                    menor = valor02;
+                }
+                else
+                {
+                    menor = valor03;
+                }
+            }
+            Console.WriteLine("O maior número é: " + maior + "O menor número é: " + menor);
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ exercicio03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do primeiro produto: ");
+            string textoProduto01 = Console.ReadLine();
+                        Console.WriteLine("Digite o valor do segundo produto: ");
+                        string textoProduto02 = Console.ReadLine();
+                        Console.WriteLine("Digite o valor do terceiro produto: ");
+                        string textoProduto03 = Console.ReadLine();
+                        
+double produto01 = Convert.ToDouble(textoProduto01);
+                        double produto02 = Convert.ToDouble(textoProduto02);
+double produto03 = Convert.ToDouble(textoProduto03);
+if(produto01 < produto03 && produto01 < produto03)
+{
+                        Console.WriteLine("Você deve comprar o produto com valor de: " + produto01);
+}
+else if(produto02 < produto01 && produto02 < produto03)
+{
+Console.WriteLine("Você deve comprar o produto com valor de: " + produto02);
+}
+else
+{
+Console.WriteLine("Você deve comprar o produto com valor de: " + produto03);
+}
+}
+}
+}
