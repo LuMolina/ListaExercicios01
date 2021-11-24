fc5d965674d49cb320f0702b5ff58289d2fec3e5
LuMolina <90227193+LuMolina@users.noreply.github.com>
2021-11-24 14:19:23 -0300

    atualizando commit

---
+++ exercicio04/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Exercicio04
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do seu salário: ");
+            string textoSalario = Console.ReadLine();
+            double salario = Convert.ToDouble(textoSalario);
+            Console.WriteLine("O valor do seu salário antes do reajuste é de> " + salario);
+            if (salario <= 2800)
+            {
+                Console.WriteLine("O seu aumento é de 20%. ");
+
+            }
+            else if (salario > 2800 && salario <= 7000)
+            {
+                Console.WriteLine("O seu aumento é de 15%. ");
+
+            }
+            else if (salario <= 15000 && salario > 7000)
+            {
+                Console.WriteLine("O seu aumento é de 10%. ");
+
+            }
+            else if (salario > 15000)
+            {
+                Console.WriteLine("O seu aumento é de 5%. ");
+
+            }
+            else
+            {
+                Console.WriteLine("Salário inelegível para aumento. ");
+
+            }
+            double salario1 = salario * 0.20;
+            double salario2 = salario * 0.15;
+            double salario3 = salario * 0.10;
+            double salario4 = salario * 0.05;
+            if (salario <= 2800)
+            {
+                Console.WriteLine("O seu aumento foi: " + salario1);
+
+            }
+            else if (salario > 2800 && salario <= 7000)
+            {
+                Console.WriteLine("O seu aumento foi de: " + salario2);
+
+            }
+            else if (salario <= 15000 && salario > 7000)
+            {
+                Console.WriteLine("O seu aumento foi de: " + salario3);
+
+            }
+            else if (salario > 1500)
+            {
+                Console.WriteLine("O seu aumento foi de: " + salario4);
+
+            }
+            else
+            {
+                Console.WriteLine("Salário inelegível para aumento. ");
+
+            }
+            double total1 = salario + salario1;
+            double total2 = salario + salario2;
+            double total3 = salario + salario3;
+            double total4 = salario + salario4;
+            if (salario <= 2800)
+            {
+                Console.WriteLine("O seu salário final é de: " + total1);
+
+            }
+            else if (salario > 2800 && salario <= 7000)
+            {
+                Console.WriteLine("O seu salário total é de: " + total2);
+
+            }
+            else if (salario <= 15000 && salario > 7000)
+            {
+                Console.WriteLine("O seu salário total é de: " + salario3);
+
+            }
+            else if (salario > 1500)
+            {
+                Console.WriteLine("O seu salário total é de: " + total4);
+
+            }
+            else
+            {
+                Console.WriteLine("Salário inelegível para aumento. ");
+
+            }
+
+        }
+    }
+}
---
+++ exercicio06/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercicio06
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor inteiro.");
+            string? textoValor01 = Console.ReadLine();
+            int valor01 = Convert.ToInt32(textoValor01);
+if (valor01 == 1)
+{
+            Console.WriteLine("O dia da semana correspondente ao valor digitado é domingo. ");
+}
+else if (valor01 == 2)
+{
+Console.WriteLine("O dia da semana correspondente ao valor digitado é segunda-feira. ");
+}
+else if (valor01 == 3)
+{
+Console.WriteLine("O dia da semana correspondente ao valor digitado é terça-feira. ");
+}
+else if (valor01 ==4)
+{
+Console.WriteLine("O dia da semana correspondente ao valor digitado é quarta-feira. ");
+}
+else if (valor01 == 5)
+{
+Console.WriteLine("O dia da semana correspondente ao valor digitado é quinta-feira. ");
+}
+else if (valor01 ==6)
+{
+Console.WriteLine("O dia da semana correspondente ao valor digitado é sexta-feira. ");
+}
+else if (valor01 == 7)
+{
+Console.WriteLine("O dia da semana correspondente ao valor digitado é sábado. ");
+}
+else
+{
+Console.WriteLine("O dia da semana correspondente ao valor digitado é inválido. ");
+}
+    }
+
+
+
+
+
+
+
+
+        }
+    }
